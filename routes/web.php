37f4f025f592520f6d262b714a50ffdfa095ceaf
h1WelTechCode/h <welwelgallardo0724@gmail.com>
2026-02-06 19:48:09 +0800

    First Commit

---
+++ routes/web.php
@@ -0,0 +1,19 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use Inertia\Inertia;
+use Laravel\Fortify\Features;
+
+Route::get('/', function () {
+    return Inertia::render('welcome', [
+        'canRegister' => Features::enabled(Features::registration()),
+    ]);
+})->name('home');
+
+Route::get('dashboard', function () {
+    return Inertia::render('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::resource('products', \App\Http\Controllers\ProductsController::class)->middleware(['auth', 'verified']);
+
+require __DIR__.'/settings.php';
